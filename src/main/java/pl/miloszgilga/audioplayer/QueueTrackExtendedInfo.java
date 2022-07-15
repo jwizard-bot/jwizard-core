6dacb6f103a34cd204a9c49cda4f397e24351d06
Milosz08 <gilgamilosz451@gmail.com>
2022-07-16 01:57:35 +0200

    create scaffold class for queue track extended info (sender track userinfo object)

---
+++ src/main/java/pl/miloszgilga/audioplayer/QueueTrackExtendedInfo.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: QueueTrackExtendedInfo.java
+ * Last modified: 15/07/2022, 03:42
+ * Project name: franek-bot
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+package pl.miloszgilga.audioplayer;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import net.dv8tion.jda.api.entities.Member;
+
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+
+
+@Getter
+@AllArgsConstructor
+public class QueueTrackExtendedInfo {
+    private Member senderUser;
+    private AudioTrack audioTrack;
+}
