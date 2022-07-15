58d6104a79e6315b3410b161ab491c50f62bc4a3
Milosz08 <gilgamilosz451@gmail.com>
2022-07-16 01:39:44 +0200

    move anonymous class to separate class and extended for more functionality

--- src/main/java/pl/miloszgilga/audioplayer/PlayerManager.java
+++ src/main/java/pl/miloszgilga/audioplayer/PlayerManager.java
@@ -18,20 +18,14 @@
 
 package pl.miloszgilga.audioplayer;
 
-import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
-import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
-import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.TextChannel;
-
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 
 public class PlayerManager {
@@ -53,31 +47,9 @@ public class PlayerManager {
         });
     }
 
-    public void loadAndPlay(TextChannel textChannel, String trackURL) {
-        final MusicManager musicManager = getMusicManager(textChannel.getGuild());
-        audioPlayerManager.loadItemOrdered(musicManager, trackURL, new AudioLoadResultHandler() {
-            @Override
-            public void trackLoaded(AudioTrack audioTrack) {
-                musicManager.getScheduler().queue(audioTrack);
-                textChannel.sendMessage("Adding to queue new track").append(audioTrack.getInfo().title).queue();
-            }
-            @Override
-            public void playlistLoaded(AudioPlaylist audioPlaylist) {
-                final List<AudioTrack> trackList = audioPlaylist.getTracks();
-                if (!trackList.isEmpty()) {
-                    musicManager.getScheduler().queue(trackList.get(0));
-                    textChannel.sendMessage("Adding to queue").append(trackList.get(0).getInfo().title).queue();
-                }
-            }
-            @Override
-            public void noMatches() {
-                textChannel.sendMessage("Nie znaleziono piosenki").queue();
-            }
-            @Override
-            public void loadFailed(FriendlyException e) {
-
-            }
-        });
+    public void loadAndPlay(CommandEvent event, String trackURL, boolean ifValidUri) {
+        final MusicManager musicManager = getMusicManager(event);
+        audioPlayerManager.loadItemOrdered(musicManager, trackURL, new AudioLoaderResult(event, ifValidUri, musicManager));
     }
 
     public static PlayerManager getSingletonInstance() {
