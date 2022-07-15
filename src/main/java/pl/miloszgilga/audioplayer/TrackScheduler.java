0dee290011d22435b8ab7da2fa3564dacef14628
Milosz08 <gilgamilosz451@gmail.com>
2022-07-15 02:53:56 +0200

    prepare TrackScheduler for repeating and shuffle queue

--- src/main/java/pl/miloszgilga/audioplayer/TrackScheduler.java
+++ src/main/java/pl/miloszgilga/audioplayer/TrackScheduler.java
@@ -23,14 +23,20 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
 import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Collections;
 
 
+@Getter
 public class TrackScheduler extends AudioEventAdapter {
 
-    public final AudioPlayer audioPlayer;
-    public final BlockingQueue<AudioTrack> queue = new LinkedBlockingQueue<>();
+    private boolean repeating = false;
+    private final AudioPlayer audioPlayer;
+    private final Queue<AudioTrack> queue = new LinkedList<>();
 
     public TrackScheduler(AudioPlayer audioPlayer) {
         this.audioPlayer = audioPlayer;
@@ -49,7 +55,19 @@ public class TrackScheduler extends AudioEventAdapter {
     @Override
     public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
         if (endReason.mayStartNext) {
-            nextTrack();
+            if (repeating) {
+                audioPlayer.startTrack(track.makeClone(), false);
+            } else {
+                nextTrack();
+            }
         }
+    }
+
+    public void setRepeating(boolean repeating) {
+        this.repeating = repeating;
+    }
+
+    public void queueShuffle() {
+        Collections.shuffle((List<?>) queue);
     }
 }
