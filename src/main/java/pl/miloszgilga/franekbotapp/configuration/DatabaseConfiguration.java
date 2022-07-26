6a7c865bae1ec5f6f19fc8ef3a8c9b3307cd12e5
Milosz08 <gilgamilosz451@gmail.com>
2022-07-26 04:06:20 +0200

    create database configuration mapping class

---
+++ src/main/java/pl/miloszgilga/franekbotapp/configuration/DatabaseConfiguration.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: DatabaseConfiguration.java
+ * Last modified: 25/07/2022, 21:28
+ * Project name: franek-bot
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+package pl.miloszgilga.franekbotapp.configuration;
+
+import lombok.Data;
+
+
+@Data
+public class DatabaseConfiguration {
+    private String databaseUrl;
+    private boolean enforceSslConnection;
+    private String username;
+    private String password;
+    private boolean createDatabaseIfNotExist;
+    private boolean sqlOnStandardOutput;
+    private String hibernateDriverPackage;
+    private String hibernateDialectPackage;
+    private String hbm2ddlAutoMode;
+}
