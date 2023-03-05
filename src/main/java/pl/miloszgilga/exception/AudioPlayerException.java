0fcbf4bb09e3cc9bcb52a2606ed19c471aff2e0b
Miłosz Gilga <gilgamilosz451@gmail.com>
2023-03-06 00:38:32 +0100

    create exceptions for lavaplayer actions

---
+++ src/main/java/pl/miloszgilga/exception/AudioPlayerException.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2023 by MILOSZ GILGA <http://miloszgilga.pl>
+ *
+ * File name: AudioPlayerException.java
+ * Last modified: 04/03/2023, 23:59
+ * Project name: jwizard-discord-bot
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+package pl.miloszgilga.exception;
+
+import lombok.extern.slf4j.Slf4j;
+
+import pl.miloszgilga.dto.EventWrapper;
+import pl.miloszgilga.core.LocaleSet;
+import pl.miloszgilga.core.configuration.BotConfiguration;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public class AudioPlayerException {
+
+    @Slf4j public static class UserOnVoiceChannelNotFoundException extends BotException {
+        public UserOnVoiceChannelNotFoundException(BotConfiguration config, EventWrapper event) {
+            super(config, LocaleSet.USER_ON_CHANNEL_NOT_FOUND_EXC.getHolder());
+            log.error("G: {}, A: {} <> Attempt to invoke command while user is not in voice channel with bot",
+                event.guildName(), event.authorTag());
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Slf4j public static class TrackIsNotPlayingException extends BotException {
+        public TrackIsNotPlayingException(BotConfiguration config, EventWrapper event) {
+            super(config, LocaleSet.TRACK_IS_NOT_PLAYING_EXC.getHolder());
+            log.error("G: {}, A: {} <> Attempt to invoke command while current played track not existing",
+                event.guildName(), event.authorTag());
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Slf4j public static class TrackIsNotPausedException extends BotException {
+        public TrackIsNotPausedException(BotConfiguration config, EventWrapper event) {
+            super(config, LocaleSet.TRACK_IS_NOT_PAUSED_EXC.getHolder());
+            log.error("G: {}, A: {} <> Attempt to invoke command while current played track is not paused",
+                event.guildName(), event.authorTag());
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Slf4j public static class MusicBotIsCurrentlyUsedException extends BotException {
+        public MusicBotIsCurrentlyUsedException(BotConfiguration config, EventWrapper event) {
+            super(config, LocaleSet.MUSIC_BOT_IS_CURRENTLY_USED_EXC.getHolder());
+            log.error("G: {}, A: {} <> Attempt to invoke command while bot is used on another channel",
+                event.guildName(), event.authorTag());
+        }
+    }
+}
---
+++ src/main/java/pl/miloszgilga/exception/BotException.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2023 by MILOSZ GILGA <http://miloszgilga.pl>
+ *
+ * File name: BotException.java
+ * Last modified: 05/03/2023, 00:00
+ * Project name: jwizard-discord-bot
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+package pl.miloszgilga.exception;
+
+import pl.miloszgilga.core.LocaleSet;
+import pl.miloszgilga.core.configuration.BotConfiguration;
+
+import java.util.Map;
+import java.util.HashMap;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public class BotException extends RuntimeException {
+
+    private final String langPattern;
+    private final BotConfiguration config;
+    private Map<String, Object> arguments = new HashMap<>();
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public BotException(BotConfiguration config, String langPattern) {
+        this.config = config;
+        this.langPattern = langPattern;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public BotException(BotConfiguration config, String langPattern, Map<String, Object> arguments) {
+        this.config = config;
+        this.langPattern = langPattern;
+        this.arguments = arguments;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public String getMessage() {
+        return config.getLocaleText(LocaleSet.findByHolder(langPattern), arguments);
+    }
+}
