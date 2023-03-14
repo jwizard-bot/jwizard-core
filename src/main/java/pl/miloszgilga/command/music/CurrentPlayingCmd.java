98835e0260c7afc8d5ce9afc10974dc9b0191b86
Miłosz Gilga <gilgamilosz451@gmail.com>
2023-03-14 14:13:08 +0100

    initialized CurrentPlaying command class

---
+++ src/main/java/pl/miloszgilga/command/music/CurrentPlayingCmd.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2023 by MILOSZ GILGA <http://miloszgilga.pl>
+ *
+ * File name: CurrentPlayingCmd.java
+ * Last modified: 14/03/2023, 04:03
+ * Project name: jwizard-discord-bot
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+package pl.miloszgilga.command.music;
+
+import lombok.extern.slf4j.Slf4j;
+
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+import pl.miloszgilga.BotCommand;
+import pl.miloszgilga.embed.EmbedMessageBuilder;
+import pl.miloszgilga.audioplayer.PlayerManager;
+import pl.miloszgilga.command.AbstractMusicCommand;
+import pl.miloszgilga.core.configuration.BotConfiguration;
+import pl.miloszgilga.core.loader.JDAInjectableCommandLazyService;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@Slf4j
+@JDAInjectableCommandLazyService
+public class CurrentPlayingCmd extends AbstractMusicCommand {
+
+    CurrentPlayingCmd(BotConfiguration config, PlayerManager playerManager, EmbedMessageBuilder embedBuilder) {
+        super(BotCommand.CURRENT_PLAYING, config, playerManager, embedBuilder);
+        super.inPlayingMode = true;
+        super.inListeningMode = true;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    protected void doExecuteMusicCommand(CommandEvent event) {
+
+    }
+}
