a93d7eaaecc6d83151565a42dd6e7f650a63ff53
milosz08 <gilgamilosz451@gmail.com>
2023-06-09 03:59:46 +0200

    replace string array into List of strings

--- src/main/java/pl/miloszgilga/BotCommand.java
+++ src/main/java/pl/miloszgilga/BotCommand.java
@@ -45,80 +45,80 @@ import static pl.miloszgilga.locale.ArgSyntaxLocaleSet.*;
 @RequiredArgsConstructor
 public enum BotCommand {
 
-    HELP                        ("help",        true, new String[]{ "h", "hl" },      OTHERS, HELP_COMMAND_DESC),
-    HELP_ME                     ("helpme",      true, new String[]{ "hm", "hlm" },    OTHERS, HELPME_COMMAND_DESC),
-    DEBUG                       ("debug",       true, new String[]{ "db", "dbg" },    OTHERS, DEBUG_COMMAND_DESC),
+    HELP                        ("help",        true, List.of("h", "hl"),      OTHERS, HELP_COMMAND_DESC),
+    HELP_ME                     ("helpme",      true, List.of("hm", "hlm"),    OTHERS, HELPME_COMMAND_DESC),
+    DEBUG                       ("debug",       true, List.of("db", "dbg"),    OTHERS, DEBUG_COMMAND_DESC),
 
-    SET_AUDIO_CHANNEL           ("setaudiochn", true, new String[]{ "sadch" },        OTHERS, SET_AUDIO_CHANNEL_COMMAND_DESC,             OPTIONAL_CHANNEL_ID_ARG_SYNTAX),
-    SET_DJ_ROLE_NAME            ("setdjrole",   true, new String[]{ "sdjr" },         OTHERS, SET_DJ_ROLE_NAME_COMMAND_DESC,              OPTIONAL_DJ_ROLE_NAME_ARG_SYNTAX),
-    SET_I18N_LOCALE             ("setlang",     true, new String[]{ "slng" },         OTHERS, SET_I18N_LOCALE_COMMAND_DESC,               OPTIONAL_I18N_LOCALE_ARG_SYNTAX),
-    SET_TRACK_REPEATS           ("settrackrep", true, new String[]{ "strrep" },       OTHERS, SET_TRACK_REPEATS_COMMAND_DESC,             OPTIONAL_TRACK_REPEATS_ARG_SYNTAX),
-    SET_DEF_VOLUME              ("setdefvol",   true, new String[]{ "sdfv" },         OTHERS, SET_DEF_VOLUME_COMMAND_DESC,                OPTIONAL_DEFAULT_VOLUME_ARG_SYNTAX),
-    SET_SKIP_RATIO              ("setskratio",  true, new String[]{ "ssrt" },         OTHERS, SET_SKIP_RATIO_COMMAND_DESC,                OPTIONAL_SKIP_RATIO_ARG_SYNTAX),
-    SET_TIME_VOTING             ("settimevot",  true, new String[]{ "stev" },         OTHERS, SET_TIME_VOTING_COMMAND_DESC,               OPTIONAL_TIME_VOTING_ARG_SYNTAX),
-    SET_TIME_LEAVE_EMPTY        ("settleavem",  true, new String[]{ "stlech" },       OTHERS, SET_TIME_LEAVE_EMPTY_COMMAND_DESC,          OPTIONAL_TIME_LEAVE_EMPTY_ARG_SYNTAX),
-    SET_TIME_LEAVE_NO_TRACKS    ("settleavetr", true, new String[]{ "stlntch" },      OTHERS, SET_TIME_LEAVE_TO_TRACKS_COMMAND_DESC,      OPTIONAL_TIME_LEAVE_NO_TRACKS_ARG_SYNTAX),
+    SET_AUDIO_CHANNEL           ("setaudiochn", true, List.of("sadch"),        OTHERS, SET_AUDIO_CHANNEL_COMMAND_DESC,             OPTIONAL_CHANNEL_ID_ARG_SYNTAX),
+    SET_DJ_ROLE_NAME            ("setdjrole",   true, List.of("sdjr"),         OTHERS, SET_DJ_ROLE_NAME_COMMAND_DESC,              OPTIONAL_DJ_ROLE_NAME_ARG_SYNTAX),
+    SET_I18N_LOCALE             ("setlang",     true, List.of("slng"),         OTHERS, SET_I18N_LOCALE_COMMAND_DESC,               OPTIONAL_I18N_LOCALE_ARG_SYNTAX),
+    SET_TRACK_REPEATS           ("settrackrep", true, List.of("strrep"),       OTHERS, SET_TRACK_REPEATS_COMMAND_DESC,             OPTIONAL_TRACK_REPEATS_ARG_SYNTAX),
+    SET_DEF_VOLUME              ("setdefvol",   true, List.of("sdfv"),         OTHERS, SET_DEF_VOLUME_COMMAND_DESC,                OPTIONAL_DEFAULT_VOLUME_ARG_SYNTAX),
+    SET_SKIP_RATIO              ("setskratio",  true, List.of("ssrt"),         OTHERS, SET_SKIP_RATIO_COMMAND_DESC,                OPTIONAL_SKIP_RATIO_ARG_SYNTAX),
+    SET_TIME_VOTING             ("settimevot",  true, List.of("stev"),         OTHERS, SET_TIME_VOTING_COMMAND_DESC,               OPTIONAL_TIME_VOTING_ARG_SYNTAX),
+    SET_TIME_LEAVE_EMPTY        ("settleavem",  true, List.of("stlech"),       OTHERS, SET_TIME_LEAVE_EMPTY_COMMAND_DESC,          OPTIONAL_TIME_LEAVE_EMPTY_ARG_SYNTAX),
+    SET_TIME_LEAVE_NO_TRACKS    ("settleavetr", true, List.of("stlntch"),      OTHERS, SET_TIME_LEAVE_TO_TRACKS_COMMAND_DESC,      OPTIONAL_TIME_LEAVE_NO_TRACKS_ARG_SYNTAX),
 
-    PLAY_TRACK                  ("play",        true, new String[]{ "p", "pl" },      MUSIC,  PLAY_TRACK_COMMAND_DESC,                    PLAY_TRACK_ARG_SYNTAX),
-    PAUSE_TRACK                 ("pause",       true, new String[]{ "ps" },           MUSIC,  PAUSE_TRACK_COMMAND_DESC),
-    RESUME_TRACK                ("resume",      true, new String[]{ "rs" },           MUSIC,  RESUME_TRACK_COMMAND_DESC),
-    REPEAT_TRACK                ("repeat",      true, new String[]{ "rp" },           MUSIC,  REPEAT_TRACK_COMMAND_DESC,                  REPEAT_TRACK_ARG_SYNTAX),
-    CLEAR_REPEAT_TRACK          ("repeatcls",   true, new String[]{ "rpcl" },         MUSIC,  CLEAR_REPEAT_TRACK_COMMAND_DESC),
-    LOOP_TRACK                  ("loop",        true, new String[]{ "lp" },           MUSIC,  LOOP_TRACK_COMMAND_DESC),
-    CURRENT_PLAYING             ("playing",     true, new String[]{ "cp" },           MUSIC,  CURRENT_PLAYING_TRACK_COMMAND_DESC),
-    CURRENT_PAUSED              ("paused",      true, new String[]{ "cps" },          MUSIC,  CURRENT_PAUSED_TRACK_COMMAND_DESC),
-    GET_PLAYER_VOLUME           ("getvolume",   true, new String[]{ "gvl" },          MUSIC,  AUDIO_PLAYER_GET_VOLUME_COMMAND_DESC),
-    QUEUE                       ("queue",       true, new String[]{ "qt" },           MUSIC,  QUEUE_COMMAND_DESC),
+    PLAY_TRACK                  ("play",        true, List.of("p", "pl"),      MUSIC,  PLAY_TRACK_COMMAND_DESC,                    PLAY_TRACK_ARG_SYNTAX),
+    PAUSE_TRACK                 ("pause",       true, List.of("ps"),           MUSIC,  PAUSE_TRACK_COMMAND_DESC),
+    RESUME_TRACK                ("resume",      true, List.of("rs"),           MUSIC,  RESUME_TRACK_COMMAND_DESC),
+    REPEAT_TRACK                ("repeat",      true, List.of("rp"),           MUSIC,  REPEAT_TRACK_COMMAND_DESC,                  REPEAT_TRACK_ARG_SYNTAX),
+    CLEAR_REPEAT_TRACK          ("repeatcls",   true, List.of("rpcl"),         MUSIC,  CLEAR_REPEAT_TRACK_COMMAND_DESC),
+    LOOP_TRACK                  ("loop",        true, List.of("lp"),           MUSIC,  LOOP_TRACK_COMMAND_DESC),
+    CURRENT_PLAYING             ("playing",     true, List.of("cp"),           MUSIC,  CURRENT_PLAYING_TRACK_COMMAND_DESC),
+    CURRENT_PAUSED              ("paused",      true, List.of("cps"),          MUSIC,  CURRENT_PAUSED_TRACK_COMMAND_DESC),
+    GET_PLAYER_VOLUME           ("getvolume",   true, List.of("gvl"),          MUSIC,  AUDIO_PLAYER_GET_VOLUME_COMMAND_DESC),
+    QUEUE                       ("queue",       true, List.of("qt"),           MUSIC,  QUEUE_COMMAND_DESC),
 
-    SET_PLAYER_VOLUME           ("setvolume",   true, new String[]{ "svl" },          DJ,     AUDIO_PLAYER_SET_VOLUME_COMMAND_DESC,       AUDIO_PLAYER_SET_VOLUME_ARG_SYNTAX),
-    RESET_PLAYER_VOLUME         ("volumecls",   true, new String[]{ "cvl" },          DJ,     AUDIO_PLAYER_RESET_VOLUME_COMMAND_DESC),
-    JOIN_TO_CHANNEL             ("join",        true, new String[]{ "jch" },          DJ,     JOIN_TO_CHANNEL_COMMAND_DESC),
-    REMOVE_MEMBER_TRACKS        ("tracksrm",    true, new String[]{ "rtr" },          DJ,     REMOVE_MEMBER_TRACKS_COMMAND_DESC,          MEMBER_TAG_ARG_SYNTAX),
-    SHUFFLE_QUEUE               ("shuffle",     true, new String[]{ "shq" },          DJ,     SHUFFLE_QUEUE_COMMAND_DESC),
-    SKIP_TO_TRACK               ("skipto",      true, new String[]{ "skt" },          DJ,     SKIP_QUEUE_TO_TRACK_COMMAND_DESC,           SKIP_QUEUE_TO_TRACK_ARG_SYNTAX),
-    SKIP_TRACK                  ("skip",        true, new String[]{ "sk" },           DJ,     SKIP_TRACK_COMMAND_DESC),
-    CLEAR_QUEUE                 ("clear",       true, new String[]{ "cl" },           DJ,     CLEAR_QUEUE_COMMAND_DESC),
-    STOP_CLEAR_QUEUE            ("stop",        true, new String[]{ "st" },           DJ,     STOP_CLEAR_QUEUE_COMMAND_DESC),
-    MOVE_TRACK                  ("move",        true, new String[]{ "mv" },           DJ,     MOVE_TRACK_COMMAND_DESC,                    MOVE_TRACK_ARG_SYNTAX),
-    INFINITE_PLAYLIST           ("infinite",    true, new String[]{ "inf" },          DJ,     INFINITE_PLAYLIST_COMMAND_DESC),
+    SET_PLAYER_VOLUME           ("setvolume",   true, List.of("svl"),          DJ,     AUDIO_PLAYER_SET_VOLUME_COMMAND_DESC,       AUDIO_PLAYER_SET_VOLUME_ARG_SYNTAX),
+    RESET_PLAYER_VOLUME         ("volumecls",   true, List.of("cvl"),          DJ,     AUDIO_PLAYER_RESET_VOLUME_COMMAND_DESC),
+    JOIN_TO_CHANNEL             ("join",        true, List.of("jch"),          DJ,     JOIN_TO_CHANNEL_COMMAND_DESC),
+    REMOVE_MEMBER_TRACKS        ("tracksrm",    true, List.of("rtr"),          DJ,     REMOVE_MEMBER_TRACKS_COMMAND_DESC,          MEMBER_TAG_ARG_SYNTAX),
+    SHUFFLE_QUEUE               ("shuffle",     true, List.of("shq"),          DJ,     SHUFFLE_QUEUE_COMMAND_DESC),
+    SKIP_TO_TRACK               ("skipto",      true, List.of("skt"),          DJ,     SKIP_QUEUE_TO_TRACK_COMMAND_DESC,           SKIP_QUEUE_TO_TRACK_ARG_SYNTAX),
+    SKIP_TRACK                  ("skip",        true, List.of("sk"),           DJ,     SKIP_TRACK_COMMAND_DESC),
+    CLEAR_QUEUE                 ("clear",       true, List.of("cl"),           DJ,     CLEAR_QUEUE_COMMAND_DESC),
+    STOP_CLEAR_QUEUE            ("stop",        true, List.of("st"),           DJ,     STOP_CLEAR_QUEUE_COMMAND_DESC),
+    MOVE_TRACK                  ("move",        true, List.of("mv"),           DJ,     MOVE_TRACK_COMMAND_DESC,                    MOVE_TRACK_ARG_SYNTAX),
+    INFINITE_PLAYLIST           ("infinite",    true, List.of("inf"),          DJ,     INFINITE_PLAYLIST_COMMAND_DESC),
 
-    GUILD_STATS                 ("gstats",      true, new String[]{ "mst" },          STATS,  GUILD_STATS_COMMAND_DESC),
-    MEMBER_STATS                ("mstats",      true, new String[]{ "gst" },          STATS,  MEMBER_STATS_COMMAND_DESC,                  MEMBER_TAG_ARG_SYNTAX),
-    MY_STATS                    ("mystats",     true, new String[]{ "myst" },         STATS,  MY_STATS_COMMAND_DESC),
-    ENABLE_STATS                ("statson",     true, new String[]{ "ston" },         STATS,  ENABLE_STATS_COMMAND_DESC),
-    DISABLE_STATS               ("statsoff",    true, new String[]{ "stoff" },        STATS,  DISABLE_STATS_COMMAND_DESC),
-    PUBLIC_STATS                ("pubstats",    true, new String[]{ "pubst" },        STATS,  PUBLIC_STATS_COMMAND_DESC),
-    PRIVATE_STATS               ("privstats",   true, new String[]{ "privst" },       STATS,  PRIVATE_STATS_COMMAND_DESC),
-    RESET_MEMBER_STATS          ("resetmstats", true, new String[]{ "rsmst" },        STATS,  RESET_MEMBER_STATS_COMMAND_DESC,            MEMBER_TAG_ARG_SYNTAX),
-    RESET_GUILD_STATS           ("resetgstats", true, new String[]{ "rsgst" },        STATS,  RESET_GUILD_STATS_COMMAND_DESC),
+    GUILD_STATS                 ("gstats",      true, List.of("mst"),          STATS,  GUILD_STATS_COMMAND_DESC),
+    MEMBER_STATS                ("mstats",      true, List.of("gst"),          STATS,  MEMBER_STATS_COMMAND_DESC,                  MEMBER_TAG_ARG_SYNTAX),
+    MY_STATS                    ("mystats",     true, List.of("myst"),         STATS,  MY_STATS_COMMAND_DESC),
+    ENABLE_STATS                ("statson",     true, List.of("ston"),         STATS,  ENABLE_STATS_COMMAND_DESC),
+    DISABLE_STATS               ("statsoff",    true, List.of("stoff"),        STATS,  DISABLE_STATS_COMMAND_DESC),
+    PUBLIC_STATS                ("pubstats",    true, List.of("pubst"),        STATS,  PUBLIC_STATS_COMMAND_DESC),
+    PRIVATE_STATS               ("privstats",   true, List.of("privst"),       STATS,  PRIVATE_STATS_COMMAND_DESC),
+    RESET_MEMBER_STATS          ("resetmstats", true, List.of("rsmst"),        STATS,  RESET_MEMBER_STATS_COMMAND_DESC,            MEMBER_TAG_ARG_SYNTAX),
+    RESET_GUILD_STATS           ("resetgstats", true, List.of("rsgst"),        STATS,  RESET_GUILD_STATS_COMMAND_DESC),
 
-    TURN_ON_STATS_MODULE        ("onstatsm",    true, new String[]{ "onstm" },        OWNER,  TURN_ON_STATS_MODULE_COMMAND_DESC),
-    TURN_OFF_STATS_MODULE       ("offstatsm",   true, new String[]{ "offstm" },       OWNER,  TURN_OFF_STATS_MODULE_COMMAND_DESC),
-    TURN_ON_MUSIC_MODULE        ("onmusicm",    true, new String[]{ "onmsm" },        OWNER,  TURN_ON_MUSIC_MODULE_COMMAND_DESC),
-    TURN_OFF_MUSIC_MODULE       ("offmusicm",   true, new String[]{ "offmsm" },       OWNER,  TURN_OFF_MUSIC_MODULE_COMMAND_DESC),
-    TURN_ON_PLAYLISTS_MODULE    ("onplaylm",    true, new String[]{ "onplm" },        OWNER,  TURN_ON_PLAYLISTS_MODULE_COMMAND_DESC),
-    TURN_OFF_PLAYLISTS_MODULE   ("offplaylm",   true, new String[]{ "offplm" },       OWNER,  TURN_OFF_PLAYLISTS_MODULE_COMMAND_DESC),
-    TURN_ON_VOTING_MODULE       ("onvotingm",   true, new String[]{ "onvtm" },        OWNER,  TURN_ON_VOTING_MODULE_COMMAND_DESC),
-    TURN_OFF_VOTING_MODULE      ("offvotingm",  true, new String[]{ "offvtm" },       OWNER,  TURN_OFF_VOTING_MODULE_COMMAND_DESC),
+    TURN_ON_STATS_MODULE        ("onstatsm",    true, List.of("onstm"),        OWNER,  TURN_ON_STATS_MODULE_COMMAND_DESC),
+    TURN_OFF_STATS_MODULE       ("offstatsm",   true, List.of("offstm"),       OWNER,  TURN_OFF_STATS_MODULE_COMMAND_DESC),
+    TURN_ON_MUSIC_MODULE        ("onmusicm",    true, List.of("onmsm"),        OWNER,  TURN_ON_MUSIC_MODULE_COMMAND_DESC),
+    TURN_OFF_MUSIC_MODULE       ("offmusicm",   true, List.of("offmsm"),       OWNER,  TURN_OFF_MUSIC_MODULE_COMMAND_DESC),
+    TURN_ON_PLAYLISTS_MODULE    ("onplaylm",    true, List.of("onplm"),        OWNER,  TURN_ON_PLAYLISTS_MODULE_COMMAND_DESC),
+    TURN_OFF_PLAYLISTS_MODULE   ("offplaylm",   true, List.of("offplm"),       OWNER,  TURN_OFF_PLAYLISTS_MODULE_COMMAND_DESC),
+    TURN_ON_VOTING_MODULE       ("onvotingm",   true, List.of("onvtm"),        OWNER,  TURN_ON_VOTING_MODULE_COMMAND_DESC),
+    TURN_OFF_VOTING_MODULE      ("offvotingm",  true, List.of("offvtm"),       OWNER,  TURN_OFF_VOTING_MODULE_COMMAND_DESC),
 
-    VOTE_SHUFFLE_QUEUE          ("vshuffle",    true, new String[]{ "vshq" },         VOTE,   VOTE_SHUFFLE_QUEUE_COMMAND_DESC),
-    VOTE_SKIP_TRACK             ("vskip",       true, new String[]{ "vsk" },          VOTE,   VOTE_SKIP_TRACK_COMMAND_DESC),
-    VOTE_SKIP_TO_TRACK          ("vskipto",     true, new String[]{ "vsto" },         VOTE,   VOTE_SKIP_TO_TRACK_COMMAND_DESC,            SKIP_QUEUE_TO_TRACK_ARG_SYNTAX),
-    VOTE_CLEAR_QUEUE            ("vclear",      true, new String[]{ "vcl" },          VOTE,   VOTE_CLEAR_QUEUE_COMMAND_DESC),
-    VOTE_STOP_CLEAR_QUEUE       ("vstop",       true, new String[]{ "vst" },          VOTE,   VOTE_STOP_CLEAR_QUEUE_COMMAND_DESC);
+    VOTE_SHUFFLE_QUEUE          ("vshuffle",    true, List.of("vshq"),         VOTE,   VOTE_SHUFFLE_QUEUE_COMMAND_DESC),
+    VOTE_SKIP_TRACK             ("vskip",       true, List.of("vsk"),          VOTE,   VOTE_SKIP_TRACK_COMMAND_DESC),
+    VOTE_SKIP_TO_TRACK          ("vskipto",     true, List.of("vsto"),         VOTE,   VOTE_SKIP_TO_TRACK_COMMAND_DESC,            SKIP_QUEUE_TO_TRACK_ARG_SYNTAX),
+    VOTE_CLEAR_QUEUE            ("vclear",      true, List.of("vcl"),          VOTE,   VOTE_CLEAR_QUEUE_COMMAND_DESC),
+    VOTE_STOP_CLEAR_QUEUE       ("vstop",       true, List.of("vst"),          VOTE,   VOTE_STOP_CLEAR_QUEUE_COMMAND_DESC);
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     private final String name;
     private final boolean slashActive;
-    private final String[] aliases;
+    private final List<String> aliases;
     private final CommandCategory category;
     private final CommandLocaleSet descriptionLocaleSet;
     private final ArgSyntaxLocaleSet argSyntax;
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    BotCommand(String name, boolean slashActive, String[] aliases, CommandCategory category, CommandLocaleSet descriptionLocaleSet) {
+    BotCommand(String name, boolean slashActive, List<String> aliases, CommandCategory category, CommandLocaleSet descriptionLocaleSet) {
         this.name = name;
         this.slashActive = slashActive;
         this.aliases = aliases;
@@ -157,7 +157,7 @@ public enum BotCommand {
         final Stream<CommandWithArgsCount> commands =  Arrays.stream(values())
             .map(v -> new CommandWithArgsCount(v.name, BotCommandArgument.count(v)));
         final Stream<CommandWithArgsCount> aliases = Arrays.stream(values())
-            .flatMap(v -> Arrays.stream(v.aliases).map(a -> new CommandWithArgsCount(a, BotCommandArgument.count(v))));
+            .flatMap(v -> v.aliases.stream().map(a -> new CommandWithArgsCount(a, BotCommandArgument.count(v))));
         return Stream.concat(commands, aliases).collect(Collectors.toSet());
     }
 
@@ -175,7 +175,7 @@ public enum BotCommand {
                 if (!command.category.equals(commandCategory)) continue;
 
                 final StringBuilder builder = new StringBuilder();
-                final String commandAliases = Arrays.stream(command.aliases)
+                final String commandAliases = command.aliases.stream()
                     .map(a -> prefix + a).collect(Collectors.joining(", "));
 
                 builder.append("`");
