070e6ced8a7e4c155a7656ec42c91c1c9546e29d
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-06-10 21:01:10 +0200

    move bot properties into separated kt data classes

---
+++ src/main/kotlin/pl/jwizard/core/bot/properties/InstanceProperties.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2024 by JWizard
+ * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
+ */
+package pl.jwizard.core.bot.properties
+
+class InstanceProperties(
+	var authToken: String = "",
+	var appId: String = "",
+	var defaultLanguage: String = "en",
+)
