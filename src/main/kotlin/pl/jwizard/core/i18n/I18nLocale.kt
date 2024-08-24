1b883f702fa7e83a9d3c5328f97655c733624344
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-08-25 00:01:55 +0200

    remove all previous source files

--- src/main/kotlin/pl/jwizard/core/i18n/I18nLocale.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2024 by JWizard
- * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
- */
-package pl.jwizard.core.i18n
-
-interface I18nLocale {
-	fun getPlaceholder(): String
-}
--- src/main/kotlin/pl/jwizard/core/radioplayback/RadioStationPlaybackFetcher.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2024 by JWizard
- * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
- */
-package pl.jwizard.core.radioplayback
-
-interface RadioStationPlaybackFetcher {
-	fun fetchData(stationSlug: String): RadioPlaybackResponseData?
-}
