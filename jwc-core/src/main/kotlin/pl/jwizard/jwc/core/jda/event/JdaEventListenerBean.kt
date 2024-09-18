20d5224e0e0361232a4164844fc6072b7cf0adb1
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-09-18 22:17:46 +0200

    minor changes

--- jwc-core/src/main/kotlin/pl/jwizard/jwc/core/jda/event/JdaEventListenerBean.kt
+++ jwc-core/src/main/kotlin/pl/jwizard/jwc/core/jda/event/JdaEventListenerBean.kt
@@ -2,7 +2,7 @@
  * Copyright (c) 2024 by JWizard
  * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
  */
-package pl.jwizard.jwc.core.jda
+package pl.jwizard.jwc.core.jda.event
 
 import org.springframework.stereotype.Component
 
