7ace13a042b638e5fdae2a20000b6930d5525395
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-02-15 03:24:54 +0100

    move InteractionRemovalThread class to transport package

---
+++ jwc-command/src/main/kotlin/pl/jwizard/jwc/command/transport/InteractionRemovalThread.kt
@@ -0,0 +1,25 @@
+package pl.jwizard.jwc.command.transport
+
+import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.interactions.components.ActionComponent
+import net.dv8tion.jda.api.interactions.components.ActionRow
+import net.dv8tion.jda.api.interactions.components.buttons.Button
+import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle
+import pl.jwizard.jwc.core.jvm.thread.JvmFixedPayloadThreadExecutor
+
+class InteractionRemovalThread : JvmFixedPayloadThreadExecutor<Message>(countOfThreads = 1) {
+
+	override fun executeJvmThreadWithPayload(payload: Message) {
+		val disabledComponents = payload.actionRows.map {
+			ActionRow.of(
+				it.actionComponents
+					.filter { component ->
+						// disable only button whose not links and any other components
+						(component is Button && component.style != ButtonStyle.LINK) || component !is Button
+					}
+					.map(ActionComponent::asDisabled)
+			)
+		}
+		payload.editMessageComponents(disabledComponents).queue()
+	}
+}
